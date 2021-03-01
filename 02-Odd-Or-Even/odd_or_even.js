13bfd03f8e8cf11acf5f321a980dc11cff08b11a
gando diallo <gandoesprit@gmail.com>
2021-03-01 11:39:25 +0100

    Solution to exercise odd_or_even

--- 02-Odd-Or-Even/odd_or_even.js
+++ 02-Odd-Or-Even/odd_or_even.js
@@ -1,7 +1,5 @@
-// odd or even reporter
-
-// write a for loop to iterate from 0 to 20
-
-  // check if the current is even or odd
-
-  // report the result to the screen
+for(var iterator = 0; iterator <= 20; iterator++){
+  if(iterator%2 == 0){
+    console.log(iterator + ' is even');
+  }
+}
