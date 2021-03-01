1868bdd0c923d0dce9ae41ccd956ccf3990cefe3
gando diallo <gandoesprit@gmail.com>
2021-03-01 11:51:07 +0100

    Solution to exercise multiplication-tables

--- 03-Multiplication-Tables/multiplication_tables.js
+++ 03-Multiplication-Tables/multiplication_tables.js
@@ -1,1 +1,6 @@
-// write your code below
+function returnResult(iterator){
+    return iterator*9;
+}
+for(var iterator = 0; iterator <= 10; iterator++){
+    console.log(iterator + ' * 9 = ' + returnResult(iterator));
+}
