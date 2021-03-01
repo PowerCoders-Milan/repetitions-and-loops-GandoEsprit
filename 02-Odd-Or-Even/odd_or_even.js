0a469f8102e6b9f3b7286f0da1e7b6b350275d29
gando diallo <gandoesprit@gmail.com>
2021-03-01 11:59:40 +0100

    Solution to exercice 02

--- 02-Odd-Or-Even/odd_or_even.js
+++ 02-Odd-Or-Even/odd_or_even.js
@@ -3,3 +3,23 @@ for(var iterator = 0; iterator <= 20; iterator++){
     console.log(iterator + ' is even');
   }
 }
+
+var iterator = 0;
+
+console.log('Loop while');
+
+while(iterator <= 20){
+  if(iterator%2 == 0){
+    console.log(iterator + ' is even');
+  }
+  iterator++;
+}
+
+console.log('Loop do..while');
+
+do{
+  if(iterator%2 == 0){
+    console.log(iterator + ' is even');
+  }
+  iterator++;
+}while(iterator <= 20)
